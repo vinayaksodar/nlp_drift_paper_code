977dfbfe80deb2ca05ac89b7ab3860ca35ad83bc
vinayaksodar <vinayakmsodar@gmail.com>
2023-06-17 18:24:31 +0530

    drift inducer now outputs perturbed data of size n

--- data_handling/drift_inducer.py
+++ data_handling/drift_inducer.py
@@ -7,45 +7,55 @@ class DriftInducer:
     A class for inducing drift in the data
     """
 
-    def __init__(self,data,target,class_label_d) :
+    def __init__(self, data, target, class_label_d):
         """
         Initialize the drift inducer.
 
         Args:
             data (pandas.DataFrame): The pandas dataframe having the data needs to be drifted
-            class_label_d(string): class label to induce
-            target(string): target column
+            class_label_d (string): class label to induce
+            target (string): target column
         """
         self.data = data
         self.class_label_d = class_label_d
-        self.target=target
-        
+        self.target = target
 
-    def adding_drift(self,n_samples,per_samp):
-         """
+    def adding_drift(self, n_sample_datasets, per_samp, sample_dataset_size=1000):
+        """
         Initialize the drift inducer.
 
         Args:
-            n_samples (int): number of samples to take for averaging the results
-            per_samp(float): percentage of the data needs to perturbed with other label
+            n_sample_datasets (int): number of sample datasets to be created.
+            per_samp (float): percentage of the data needs to be perturbed with the other label
+
+        Returns:
+            df_samples(List[Pandas.DataFrames]) : A list of randomly sampled datasets with per_samp of induced drift 
         """
-         
-         df = self.data
-         target = self.target
-         label = self.class_label_d
 
-         df_orig=df[df[target]!=label]
-         drift_indx = df[df[target]==label].index.tolist()
-         df_samples=[]
+        df = self.data
+        target = self.target
+        label = self.class_label_d
 
+        df_orig = df[df[target] != label]
+        drift_indx = df[df[target] == label].index.tolist()
+        df_samples = []
 
-         for i in range(n_samples):
-             k=int(per_samp*len(drift_indx))
-             random.seed(i)
-             smp_list = random.sample(drift_indx,k)
+        for i in range(n_sample_datasets):
+            seed = i
+            np.random.seed(i)
+            df_sampled = df_orig.sample(n=sample_dataset_size, random_state=seed)
 
-             df_ind = df.iloc[smp_list,:]
-             df_ind = df_orig.append(df_ind)
+            #Remove the percentage of data specified by user
+            num_values_to_remove = int(per_samp * len(df_sampled))
+            indices_to_remove = np.random.choice(df_sampled.index, num_values_to_remove, replace=False)
+            df_sampled = df_sampled.drop(indices_to_remove)
 
-             df_samples.append(df_ind)
-         return df_samples
+            #Add the samme percentage of data with different label
+            np.random.seed(i+100)
+            indices_to_add = np.random.choice(drift_indx, num_values_to_remove, replace= False)
+            df_ind = df.loc[indices_to_add]
+            df_ind = df_sampled.append(df_ind)
+
+            df_samples.append(df_ind)
+
+        return df_samples
